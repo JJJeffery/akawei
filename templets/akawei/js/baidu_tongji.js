4ff3caeebeed758550f2a96c9d88e0fc89696013
Jeffery <heacheuy@live.cn>
2017-03-18 16:19:02 +0800

    add baidu tongji

---
+++ templets/akawei/js/baidu_tongji.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Jeffery on 2017/3/18.
+ * 百度统计代码
+ */
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?21d89723f79a7919e126681f47849d32";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
